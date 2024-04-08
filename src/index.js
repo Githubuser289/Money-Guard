fe483d14fb724ba75c1353c1f9461f4f80341319
Githubuser289 <moldovan.mms@gmail.com>
2024-04-08 11:35:53 +0300

    adjust

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme.js';
-import { GlobalStyles } from './styles/GlobalStyles';
+// import { GlobalStyles } from './styles/GlobalStyles';
 import './index.css';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './redux/store.js';
