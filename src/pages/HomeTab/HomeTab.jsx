351015278db6d7e69861f14a3ff59d1fff27d952
Githubuser289 <moldovan.mms@gmail.com>
2024-04-08 11:29:13 +0300

    small adjust

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -2,7 +2,7 @@
 // import MobileList from '../../components/TransactionsList/MobileList';
 import TransactionsList from '../../components/TransactionsList/TransactionsList';
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+// import { useMediaQuery } from 'react-responsive';
 import { StyledHomeTabContainer } from './HomeTab.styled';
 
 const HomeTab = () => {
